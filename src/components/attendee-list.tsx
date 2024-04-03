7bf35d9715d1dcaebd0f130bef6b8120c4aca49e
Thiago <thiago.marim2005@gmail.com>
2024-04-03 14:30:43 -0300

    feat: added an Icon Button component

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -6,6 +6,7 @@ import {
   MoreHorizontal,
   Search,
 } from "lucide-react";
+import { IconButton } from "./icon-button";
 
 export function AttendeeList() {
   return (
@@ -78,9 +79,9 @@ export function AttendeeList() {
                     3 dias atrás
                   </td>
                   <td className="py-3 px-4 text-sm text-zinc-300">
-                    <button className="bg-black/20 border border-white/10 rounded-md p-1.5">
-                      <MoreHorizontal />
-                    </button>
+                    <IconButton transparent={true}>
+                      <MoreHorizontal className="size-4" />
+                    </IconButton>
                   </td>
                 </tr>
               );
@@ -97,18 +98,18 @@ export function AttendeeList() {
               <div className="inline-flex items-center gap-8">
                 <span>Página 1 de 23</span>
                 <div className="flex gap-1.5">
-                  <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                  <IconButton>
                     <ChevronsLeft className="size-4" />
-                  </button>
-                  <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                  </IconButton>
+                  <IconButton>
                     <ChevronLeft className="size-4" />
-                  </button>
-                  <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                  </IconButton>
+                  <IconButton>
                     <ChevronRight className="size-4" />
-                  </button>
-                  <button className="bg-white/10 border border-white/10 rounded-md p-1.5">
+                  </IconButton>
+                  <IconButton>
                     <ChevronsRight className="size-4" />
-                  </button>
+                  </IconButton>
                 </div>
               </div>
             </td>
